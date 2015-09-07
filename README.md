ce376c176033821b88737df2c849ca73d3390ec0
Torben Knerr <mail@tknerr.de>
2015-09-07 17:13:08 +0200

    initial state after generating

---
+++ README.md
@@ -0,0 +1,4 @@
+# mean-dev-vm
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'mean-dev-vm'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures mean-dev-vm'
+long_description 'Installs/Configures mean-dev-vm'
+version '0.1.0'
